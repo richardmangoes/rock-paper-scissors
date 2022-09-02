f541633ae595f2b74887fbcd84a81ba7ad8e4901
RichardMangoes <riccardo.manghi@yahoo.it>
2022-09-02 12:16:55 +0200

    Added functions: computerChoice, initial capital letter

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
+//Declare the possible choices of RPS
 const choice = ['Rock','Paper','Scissors'];
 console.log(choice);
 
+//Randomize the computer choice
 function getComputerChoice() {
-    let compIndex = Math.floor(Math.random() * 3);
-    let compChoice = choice[compIndex];
-    console.log(compChoice);
+    let computerIndex = Math.floor(Math.random() * 3);
+    return choice[computerIndex];
 }
 
-getComputerChoice();
-getComputerChoice();
-getComputerChoice();
-getComputerChoice();
-getComputerChoice();
+function initialCapital(text) {
+    text = text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+    return text;
+}
+
+//Single round function
+//function playRound(playerSelection, computerSelection) {
+//    if 
+//}
+
+console.log(getComputerChoice());
+console.log(initialCapital("gINO"));
