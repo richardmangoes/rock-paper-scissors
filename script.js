c4250da538fad95344602e44fdf2cc477c9c44fa
RichardMangoes <riccardo.manghi@yahoo.it>
2022-09-02 15:59:36 +0200

    Fixed function playRound and added game function, for 5 games in a row

--- script.js
+++ script.js
@@ -16,56 +16,78 @@ function initialCapital(text) {
 
 //Single round function
 function playRound(playerSelection, computerSelection) {
-    let result;
     switch (initialCapital(playerSelection)) {
         case "Rock":
             switch (computerSelection) {
                 case "Rock": 
-                    result = "Draw! Rock draws vs Rock";
+                    result = "Draw! You both played Rock";
                     break;
                 case "Scissors":
-                    result = "You won!";
-                    playerPoints++;
+                    result = "You won! Rock beats Scissors";
                     break;
                 case "Paper":
-                    result = "You lost!";
-                    computerPoints++;
+                    result = "You lost! Paper beats Rock";
                     break;
             }
         break;
         case "Scissors":
             switch (computerSelection) {
                 case "Scissors": 
-                    result = "Draw!";
+                    result = "Draw! You both played Scissors";
                     break;
                 case "Paper":
-                    result = "You won!";
-                    playerPoints++;
+                    result = "You won! Scissors beats Paper";
                     break;
                 case "Rock":
-                    result = "You lost!";
-                    computerPoints++;
+                    result = "You lost! Rock beats Scissors";
                     break;
             }
         break;
         case "Paper":
             switch (computerSelection) {
                 case "Paper": 
-                    result = "Draw!";
+                    result = "Draw! You both player Paper";
                     break;
                 case "Rock":
-                    result = "You won!";
-                    playerPoints++;
+                    result = "You won! Paper beats Rock";
                     break;
                 case "Scissors":
-                    result = "You lost!";
-                    computerPoints++;
+                    result = "You lost! Scissors beats Paper";
                     break;
             }
        break;
+       default:
+           result = "Wrong input!"
+           break;
     }
-    
+    return result;
 }
 
-console.log(getComputerChoice());
-console.log(initialCapital("gINO"));
+//5 games in a row!!
+function game() {
+    let computerPoints = 0;
+    let playerPoints = 0;
+    for (let i = 0; i < 5; i++) {
+        playerSelection = initialCapital(prompt("Choose Rock, Paper or Scissors!"));
+        playRound(playerSelection, getComputerChoice());
+        console.log(result);
+        if (result.includes("won")) {
+            playerPoints++;
+        } else if (result.includes("lost")) {
+            computerPoints++;
+        } else {
+            playerPoints++;
+            computerPoints++;
+        }
+    }
+    if (playerPoints > computerPoints) {
+        finalScore = `Congratulations, you won! Final score is ${playerPoints} to ${computerPoints}`;
+    } else if (playerPoints < computerPoints) {
+        finalScore = `I'm sorry, you lost! Final score is ${playerPoints} to ${computerPoints}`;
+    } else {
+        finalScore = `The game was a draw! Final score is ${playerPoints} even!`
+    }
+    console.log(finalScore);
+}
+
+console.log(game());
